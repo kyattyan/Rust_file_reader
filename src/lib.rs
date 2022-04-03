888b93e992de940e4dc8631c9bfd1f76c0f1c38d
Koichi Yatsuduka <k.yattyan349245@gmail.com>
2022-04-03 16:02:18 +0900

    g09出力からSCFエネルギー抜き出し
    出力ファイルからSCF doneを抽出して列挙する部分を作成

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,25 @@
-use std::error::Error;
+use std::error;
 use std::fs::File;
-use std::io::prelude::*;
+use std::collections::VecDeque;
+use std::io::{prelude::*};
+
+//To do:　簡単なプロット作成
 
 pub struct Config {
     pub query: String,
     pub filename: String,
 }
 
+pub enum Software{
+    G09,
+    G16,
+}
+
 impl Config {
+    
+    /*実行時のコマンドに与えられた引数の数を確認する。
+    足りなければErrを返す。
+    受け取り側でパニックさせる*/
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3{
             return Err("not enough arguments");
@@ -19,21 +31,69 @@ impl Config {
     }
 } 
 
-pub fn run(config: Config) -> Result<(), Box<Error>>{
+/*メイン部分*/
+pub fn run(config: Config) -> Result<(), Box<dyn error::Error>>{
     // ファイルが見つかりませんでした
     let mut f = File::open(config.filename)?;
 
+    //ファイルの中身をいったん全部保存する
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
+
+    let mut numerical_scf_done_lines: VecDeque<f64>;
+
+    let mut scf = Vec::<f64>::new();
+
     for line in search(&config.query, &contents){
         println!("{}", line);
+        /*for numbers in line.matches(char::is_numeric).collect(){
+            numerical_scf_done_lines.push(numbers);
+        }*/
+        numerical_scf_done_lines = line.split_whitespace().filter_map(|k| k.parse().ok()).collect::<VecDeque<f64>>();
+
+
+        match numerical_scf_done_lines.pop_front() {
+            Some(x) => scf.push(x),
+            None => println!("Error: Loading SCF energy was failed")
+        }
+
+        /*for num in numerical_scf_done_lines {
+            println!("{:.9}", num);
+        }*/
     }
+
+    {
+        let mut i:i32 = 1;
+    
+        for opt_energy in scf{
+            println!("{}: {:.9}",i, opt_energy);
+            i+=1;
+        }
+    }
+
 
     // テキストは\n{}です
     //println!("With text:\n{}", contents);
 
     Ok(())
+}
+
+/*出力ファイルの最初の数行から、出力してきたソフトウェアを識別する
+識別できなかった場合はエラーを返すのでパニックする。
+パニックするときにファイル名を受け取り側で明記する。
+ひとまずはハードコーディング */
+pub fn identify_calculation_software(contents: &str) -> Result<Software, &'static str>{
+    let contents_lines = contents.lines();
+    for line in contents_lines{
+        if line.contains("G09"){
+            return Ok(Software::G09);
+        }
+        if line.contains("G16"){
+            return Ok(Software::G16);
+        }
+    }
+    Err("cannot identify software which output this file")
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -62,6 +122,10 @@ pub fn extension_identifier(file_name: &str) -> Option<&str>{
     
 
     results.pop()
+}
+
+pub fn write_summary_to_file(){
+
 }
 
 #[cfg(test)]
